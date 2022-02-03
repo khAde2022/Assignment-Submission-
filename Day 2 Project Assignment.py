9df80d59541d09df6e59150a2d62d4b191cc41cf
khAde2022 <98941566+khAde2022@users.noreply.github.com>
2022-02-03 15:52:37 +0530

    Add files via upload

---
+++ Day 2 Project Assignment.py
@@ -0,0 +1,8 @@
+word='mangekyo sharingan'
+print(word)
+l=input("Select any one alphabhet from given words:")
+lo=['m','a','n','g','e','k','y','o','s','h','r','i']
+if l==lo[4] or l==lo[6] or l==lo[8]:
+    print("You have won the lottery")
+else :
+    print("Better luck next time")
